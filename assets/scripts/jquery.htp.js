d9e27ddd97c4abe54f81a82b19691d097590d2dc
Sung Kwang David Kim <sungkwangkim@gmail.com>
2017-01-14 22:47:33 -0800

    move mouse alert does not activate after game has ended

--- assets/scripts/jquery.htp.js
+++ assets/scripts/jquery.htp.js
@@ -3,21 +3,23 @@ var timeout = null;
 // making the alert show
 $(function() {
   $('#start').on('click', function(){
-    console.log("yes");
 
     $('#canvas').on('mousemove', function(){
       clearTimeout(timeout);
 
-      timeout = setTimeout(function() {
-        $('.moveDect').removeClass('v-h');
-      }, 1500);
+        timeout = setTimeout(function() {
+          if($('#game-over').hasClass('v-h')){
+            $('.moveDect').removeClass('v-h');
+          }
+        }, 1500);
     });
   });
 });
 
 // making the alert disappear
+
 $(function() {
   $('#canvas').on('mousemove', function(){
-        $('.moveDect').addClass('v-h');
+      $('.moveDect').addClass('v-h');
   });
 });
