b97a461415cbdc14244b3ab2c866bb09dbdacb4c
Sung Kwang David Kim <sungkwangkim@gmail.com>
2017-01-14 20:20:35 -0800

    alert doesn't show up on start screen

--- assets/scripts/jquery.htp.js
+++ assets/scripts/jquery.htp.js
@@ -1,22 +1,17 @@
 var timeout = null;
 
-// $(document).on('mousemove', function() {
-//     clearTimeout(timeout);
-//
-//     timeout = setTimeout(function() {
-//         console.log('Mouse idle for 3 sec');
-//     }, 3000);
-// });
-
 // making the alert show
 $(function() {
-  $('#canvas').on('mousemove', function(){
-        clearTimeout(timeout);
+  $('#start').on('click', function(){
+    console.log("yes");
 
+    $('#canvas').on('mousemove', function(){
+      clearTimeout(timeout);
 
-        timeout = setTimeout(function() {
-            $('.moveDect').removeClass('v-h');
-        }, 1500);
+      timeout = setTimeout(function() {
+        $('.moveDect').removeClass('v-h');
+      }, 1500);
+    });
   });
 });
 
@@ -26,14 +21,3 @@ $(function() {
         $('.moveDect').addClass('v-h');
   });
 });
-
-
-// $(function() {
-//   $('.js').on('mouseover', function(){
-//     $('.js').siblings().removeClass('hidden flipOutX').addClass('animated flipInX');
-//
-//     $('.js').one('mouseleave', function(){
-//       $('.js').siblings().removeClass('animated flipInX').addClass('animated flipOutX');
-//     });
-//   });
-// });
