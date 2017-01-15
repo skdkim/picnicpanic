c3fcf35b2fc615564526a707c819b28b37e5e80c
Sung Kwang David Kim <sungkwangkim@gmail.com>
2017-01-14 19:53:55 -0800

    make alert show at proper timing

--- assets/scripts/jquery.htp.js
+++ assets/scripts/jquery.htp.js
@@ -8,12 +8,32 @@ var timeout = null;
 //     }, 3000);
 // });
 
+// making the alert show
 $(function() {
-  $(document).on('mousemove', function(){
+  $('#canvas').on('mousemove', function(){
         clearTimeout(timeout);
 
+
         timeout = setTimeout(function() {
-            console.log('Mouse idle for 3 sec');
-        }, 3000);
+            $('.moveDect').removeClass('v-h');
+        }, 1500);
   });
 });
+
+// making the alert disappear
+$(function() {
+  $('#canvas').on('mousemove', function(){
+        $('.moveDect').addClass('v-h');
+  });
+});
+
+
+// $(function() {
+//   $('.js').on('mouseover', function(){
+//     $('.js').siblings().removeClass('hidden flipOutX').addClass('animated flipInX');
+//
+//     $('.js').one('mouseleave', function(){
+//       $('.js').siblings().removeClass('animated flipInX').addClass('animated flipOutX');
+//     });
+//   });
+// });
